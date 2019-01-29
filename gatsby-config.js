bc8e91329ac5d5208c8544cf7238fb18b84aa11d
sarasate <luis@superlumen.io>
2019-01-29 17:09:29 +0100

    chore: change site meta data

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `Gate API Doc Generator`,
+    description: `Generate api docs with GatsbyJS and Markdown.`,
     author: `@gatsbyjs`,
   },
   plugins: [

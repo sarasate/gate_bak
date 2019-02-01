dae4d12898da812186294d7dec22de3c9cc86f8f
sarasate <luis@superlumen.io>
2019-02-01 23:55:12 +0100

    refactor: move render function in own file

--- src/components/page/page.js
+++ src/components/page/page.js
@@ -1,37 +1,8 @@
 import React from 'react'
 import styled from 'styled-components'
-import { Nodes } from '../../node-mapping'
+import { renderNodes } from '../../node-renderer'
 
 import CodeBox from '../code-box'
-
-const renderNodes = (nodes, index) => {
-  return nodes.map(node => {
-    if (node.type === 'element') {
-      const tag = Nodes[node.tagName] || Nodes['default']
-
-      // Render links
-      if (node.tagName === 'a')
-        return tag.render({
-          children: renderNodes(node.children),
-          href: node.properties.href,
-        })
-      const language = node.properties && node.properties.dataLanguage
-      // Check for code language examples
-      const active =
-        typeof window !== 'undefined' &&
-        node.tagName === 'div' &&
-        (language === 'json' || language === window.language)
-      const element = tag.render({
-        children: renderNodes(node.children),
-        className: node.properties.className,
-        active,
-      })
-      return element
-    } else if (node.type === 'text') {
-      return node.value
-    }
-  })
-}
 
 const Page = ({ content }) => {
   return (
---
+++ src/node-renderer.js
@@ -0,0 +1,30 @@
+import { Nodes } from './node-mapping'
+
+export const renderNodes = nodes => {
+  return nodes.map(node => {
+    if (node.type === 'element') {
+      const tag = Nodes[node.tagName] || Nodes['default']
+
+      // Render links
+      if (node.tagName === 'a')
+        return tag.render({
+          children: renderNodes(node.children),
+          href: node.properties.href,
+        })
+      const language = node.properties && node.properties.dataLanguage
+      // Check for code language examples
+      const active =
+        typeof window !== 'undefined' &&
+        node.tagName === 'div' &&
+        (language === 'json' || language === window.language)
+      const element = tag.render({
+        children: renderNodes(node.children),
+        className: node.properties.className,
+        active,
+      })
+      return element
+    } else if (node.type === 'text') {
+      return node.value
+    }
+  })
+}
